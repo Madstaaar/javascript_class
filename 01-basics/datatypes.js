aa838ac62d6ea767e51822867018ab95051c0a16
Madstaaar <47496540+Madstaaar@users.noreply.github.com>
2024-05-02 09:16:42 +0000

    all done

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,12 @@
+let name="ibrahim"
+let email="enquiryforme286@gmail.com"
+let phone="9325729795"
+let id=4568
+let married=false
+let position //in js null and undefined are different things
+let weight=71.19
+
+
+
+// console.table //print data in tabular format
+console.table([name,email,phone,id,married,position,weight])
---
+++ 01-basics/start.js
@@ -0,0 +1,10 @@
+let a=223
+// const a="64" //not allowed
+const b=a //this is allowed
+console.log(a ,b); //work fine
+console.log(a==b); //both are same
+a=65
+// b=a  //cant change constant
+console.log(a,b);
+
+
