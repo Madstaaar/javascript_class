d6c6db32293cac65599e1da480e2cbcfd141f1d0
Madstaaar <47496540+Madstaaar@users.noreply.github.com>
2024-05-03 19:50:50 +0000

    conversion in js

---
+++ 01-basics/conversion.js
@@ -0,0 +1,15 @@
+/*CONVERSION IN JS */
+
+
+let a="33"
+console.log(typeof a)
+console.log(a);
+
+let convertString=Number(a)
+console.log(typeof convertString);
+console.log(convertString);
+
+let convertBool=Boolean(a)
+console.log(typeof convertBool);
+console.log(convertBool);
+
