f7f09f84cf9334128b61b991f4e5f8cf85cc89a2
Madstaaar <47496540+Madstaaar@users.noreply.github.com>
2024-05-03 20:00:12 +0000

    conversion in js

--- 01-basics/conversion.js
+++ 01-basics/conversion.js
@@ -1,15 +1,41 @@
-/*CONVERSION IN JS */
+/*CONVERSIONS IN JS */
 
-
-let a="33"
-console.log(typeof a)
-console.log(a);
+let a="33f"
+                 //when convert this in number it becomes number but gives NaN ie not a number
+console.log(a)
+console.log(typeof a);
 
 let convertString=Number(a)
 console.log(typeof convertString);
 console.log(convertString);
+                            /*STRING TO BOOLEAN */
+let count=" "
+console.log(count)
+console.log(typeof count)
+                // covert empty string to boolean gives value of false and for whitespace or string it is true
 
-let convertBool=Boolean(a)
-console.log(typeof convertBool);
-console.log(convertBool);
+let converBool=Boolean(count)
+console.log(converBool)
+console.log(typeof converBool)
+                            /*NUMBER TO BOOLEAN */
 
+let numb=0
+console.log(typeof numb)
+console.log(numb);
+
+                /*converting number to bool every nonzero is true and zero val is false*/
+
+let numToBool=Boolean(numb)
+console.log(typeof numToBool)
+console.log(numToBool);
+
+let gh=true
+let boolToNum=Number(gh)
+console.log(typeof boolToNum);
+console.log(boolToNum);
+
+            /*BOOLEAN TO STRING */
+let flag=true
+let boolToString=String(flag)
+console.log(typeof boolToString);
+console.log(boolToString==flag);
--- 01-basics/datatypes.js
+++ 01-basics/datatypes.js
@@ -10,3 +10,14 @@ let weight=71.19
 
 // console.table //print data in tabular format
 console.table([name,email,phone,id,married,position,weight])
+
+//for knowing the type of variable typeof keyword and function is used
+console.log(typeof name)
+console.log(typeof email)
+console.log(typeof phone)
+console.log(typeof id)
+console.log(typeof married)
+console.log(typeof(position))   //also written in the form of method
+console.log(typeof weight)
+
+
