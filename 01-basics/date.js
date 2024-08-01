80cbf39b7cb091d46ff3e0c1d675211871cff337
Madstaaar <47496540+Madstaaar@users.noreply.github.com>
2024-08-01 20:08:10 +0000

    half done

---
+++ 01-basics/date.js
@@ -0,0 +1,15 @@
+// gettime
+// /1000
+// now()
+let mDate=new Date()
+console.log(mDate); //printing normally give2024-08-01T16:10:12.993Z
+console.log(mDate.toDateString());//Thu Aug 01 2024
+console.log(mDate.toISOString());//2024-08-01T16:13:36.841Z
+console.log(mDate.toLocaleDateString());//8/1/2024
+console.log(mDate.toLocaleString());//8/1/2024, 5:02:57 PM
+console.log(mDate.toJSON());//2024-08-01T17:02:57.842Z
+console.log(mDate.toString());//Thu Aug 01 2024 17:02:57 GMT+0000 (Coordinated Universal Time)
+// console.log(mDate.getDate)
+
+
+
